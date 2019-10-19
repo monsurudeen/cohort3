0ccdcde1572353e94b1db8ef3245b92661bac89e
monsurudeen <delekasim@gmail.com>
2019-10-19 12:58:58 -0600

    updating daily.js and daily.test.js

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -13,8 +13,170 @@
 
 */
 const functions = {
+
+  //----------- DAILY EXERCISES- MORE ARRAY WORK: 2019-10-16------------
+
+ // A sample function that uses the "slice()" function on an array...
+
+
+ sliceFn: (arr) => {  // arr is an array...
+
+  return arr.slice(0,2); // returns the first 2 elements of the array..
+
+},
+
+// A sample function that uses the "splice()" function on an array..
+
+spliceFn: (arr, elt) => {
+       arr.splice(2,0, elt); // Adds one element at index no 2, removed none..
+         return arr;  //returns the array with the added element
+},
+
+// A sample function that uses "for-each()" function on an array..
+
+forEachFn:  (arr) => {  // arr is an array
+        const arr1 = [];   // 
+
+        arr.forEach( (elt) => {
+            arr1.push(elt);   // push the elemments of arr into arr1
+            
+        })
+         
+        return arr1;
+
+},
+
+// A sample fnction that uses "map()" function on an array....
+
+arrayMap:  (arr) => {
+        const arr1 = arr.map( (elt) => {
+                return elt + "?";  // returns the elements in an array with a "?" appended..
+              })
+      return arr1;
+},
+
+// A sample fnction that uses "reduce()" function on an array...
+
+arrayFilter: (arr) => {
+       const arr1 =  arr.filter( (elt) => {
+                return elt > 2;   // returns only the elements greater than 2..
+              })
+        return arr1;
+
+},
+
+// A sample fnction that uses "filter()" function on an array...
+
+arrayReduce:  (arr) => {
+        const arr1 =  arr.reduce( (acc, elt) => {
+               return acc + elt;  // returns the sum of all elements in the array
+             }, 0 )
+
+        return arr1;
+},
+
+// A sample fnction that uses "sort()" function on an array...
+
+arraySort:   (arr) => { return arr.sort();
+
+},
+
     
-   /* assertEquality: (p1,p2) => {
+   
+ //---------- DAILY EXERCISES: WORKING WITH ARRAYS: 2019-10-15--------------
+
+      // A  sample while function with an array ...
+
+      whileTest: (a) => {        // a is an array..
+        let sum = 0;
+        let counter = 0;
+        while(counter <= a.length){
+            sum+=1;
+            counter++;
+        } 
+        return sum;
+  
+      },
+  
+      // A sample "For" function with an array....
+  
+      forTest: (b) => {        // b is an array...
+        let sum = 0;
+        for(let i = 0; i <= b.length; i++){
+             
+             sum+=1;
+        }
+        return sum;
+       
+      },
+      
+      // A sample "do-while" function with an array...
+  
+      doWhileTest: (c) => {       //  c is an array....
+        let sum = 0;
+        let counter = 0;
+  
+        do{
+         sum+=1;
+         counter++;
+        } while(counter <= c.length)
+                 
+        return sum;
+  
+        },
+  
+  
+        // A sample "for-in" function with an array...
+  
+        forIn: (ind) => {  // ind is an array......
+           const arr = [];
+          for(const i in ind){
+             arr.push(i);
+          }
+  
+          return arr;
+        },
+  
+  
+        // A sample "for-of" function with an array....
+  
+        forOf: (val)  => { // val is an array...
+           const arr = [];
+           for(const i of val){
+             arr.push(i);
+           }
+           return arr;
+        },
+
+
+  
+//-----------DAILY EXERCISES: 2019-10-11 ---------------
+
+makeEmailObj: (name) => {
+  // use the map function to create a new array from the "name" object
+  //values and return them in lowercase
+
+   const newArr = Object.values(name). map((names) => { return names.toLowerCase();})
+
+     let first = newArr[0];
+     let last = newArr[1];
+
+     return  (first + "." + last + "@evolveu.ca");
+},
+
+
+makeEmailArr: (name) => {
+
+  let first = name[0].toLowerCase();
+  
+   let last = name[1].toLowerCase();
+       
+ return (first + "." + last + "@evolveu.ca");
+},
+
+
+
+assertEquality: (p1,p2) => {
         if(p1 === p2) {return "true";}
 
          else {
@@ -24,38 +186,13 @@ const functions = {
 
              return "false";
          }
-        
-        
     },
 
 
-    makeEmailArr: (name) => {
+   
 
-        let first = name[0].toLowerCase();
-        
-         let last = name[1].toLowerCase();
-  
   
        
-       return (first + "." + last + "@evolveu.ca");
-  
-  }*/
-
-
-   makeEmailObj: (name) => {
-    // use the map function to create a new array from the "name" object
-    //values and return them in lowercase
-
-     const newArr = Object.values(name). map((names) => { return names.toLowerCase();})
-
-       let first = newArr[0];
-       let last = newArr[1];
-
-       return  (first + "." + last + "@evolveu.ca");
-  }
-  
-  
-
 
 };
 
--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -1,6 +1,72 @@
 import functions from './daily'
 
-/*test('Are the two values equal', () => {
+// Daily Exercises : MORE ARRAY WORKS: 2019-10-16....
+
+test('more array works', () => {
+
+    const loop2 = [1,2,3,4,5];
+    const loop3 = ["cat","dog","musu"];
+    const loop4 = [5,4,3,2,1];
+    
+    expect(functions.sliceFn(loop3)).toEqual(["cat","dog"]); 
+    expect(functions.forEachFn(loop3)).toEqual(["cat","dog","musu"]);
+    expect(functions.arrayMap(loop3)).toEqual(["cat?","dog?","musu?"]);
+    expect(functions.spliceFn(loop3, "elephant")).toEqual(["cat","dog","elephant","musu"]);
+    expect(functions.arrayFilter(loop2)).toEqual([3,4,5]); 
+    expect(functions.arrayReduce(loop2)).toEqual(15);
+    expect(functions.arraySort(loop4)).toEqual([1,2,3,4,5]);
+    });
+
+
+// Daily Exercises: PREPARE FOR ARRAY WORK.:2019-10-15......
+
+
+test('prepare for array work', () => {
+
+    const loop = [1,2,3];
+    const loop1 = ["cat","dog","musu"];
+    
+    expect(functions.whileTest(loop)).toBe(4); 
+    expect(functions.forTest(loop)).toBe(4);
+    expect(functions.doWhileTest(loop)).toBe(4);
+    expect(functions.forIn(loop1)).toEqual(["0","1","2"]); // test should return the indices
+    expect(functions.forOf(loop1)).toEqual(["cat","dog","musu"]); // test should return the values
+   
+    });
+
+//Write the function to format an email based on an object / map
+
+
+test('email builder from an object / map', () => {
+
+    const name = { fname: 'first', lname: 'last' };
+    expect(functions.makeEmailObj(name))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailObj({ fname: 'First', lname: 'Last' }))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailObj({ fname: "Bill", lname: "Smith" }))
+        .toEqual("bill.smith@evolveu.ca");
+    });
+    
+   
+ //Write a function to format an email based on an array.
+
+
+test('email builder from an array', () => {
+
+    const name = ["first", "last"];
+    expect(functions.makeEmailArr(name))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailArr(["First", "Last"]))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailArr(["Bill", "Smith"]))
+        .toEqual("bill.smith@evolveu.ca");
+    });
+     
+
+
+
+test('Are the two values equal', () => {
    
     expect(functions.assertEquality("a", "b")).toBe("false"); 
     expect(functions.assertEquality("a", "a")).toBe("true");
@@ -10,35 +76,12 @@ import functions from './daily'
     expect(functions.assertEquality("This value", "This value")).toBe("true");
 });
 
-/*
-Write a function to format an email based on an array.
-*/
-
-/*test('email builder from an array', () => {
-const name = ["first", "last"];
-expect(functions.makeEmailArr(name))
-    .toEqual("first.last@evolveu.ca");
-expect(functions.makeEmailArr(["First", "Last"]))
-    .toEqual("first.last@evolveu.ca");
-expect(functions.makeEmailArr(["Bill", "Smith"]))
-    .toEqual("bill.smith@evolveu.ca");
-});
 
 
 
-/*	
-Write the function to format an email based on an object / map
-*/
 
-test('email builder from an object / map', () => {
-const name = { fname: 'first', lname: 'last' };
-expect(functions.makeEmailObj(name))
-    .toEqual("first.last@evolveu.ca");
-expect(functions.makeEmailObj({ fname: 'First', lname: 'Last' }))
-    .toEqual("first.last@evolveu.ca");
-expect(functions.makeEmailObj({ fname: "Bill", lname: "Smith" }))
-    .toEqual("bill.smith@evolveu.ca");
-});
+	
+
 
 
 
