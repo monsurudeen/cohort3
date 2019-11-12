dfe5d03f17449784345b339fc151fb17833d1b86
monsurudeen <delekasim@gmail.com>
2019-11-11 19:04:53 -0700

    130A Accounts class created and tested

---
+++ src/03-objects/scripts/accounts.js
@@ -0,0 +1,42 @@
+
+class Accounts {
+    constructor(accName, bal) {
+        this.accName = accName;
+        this.bal = bal;
+    }
+
+    deposit(depAmt) {
+
+        if (depAmt <= 0) {
+            let error = `Deposit Amt must be greater than 0`;
+            console.log(error);
+            return error;
+        } else {
+            this.bal += depAmt;
+            return this.bal;
+        }
+    }
+
+    withdraw(withdrawAmt) {
+
+        if (withdrawAmt<= 0 || withdrawAmt>= this.bal) {
+            let error = `Withdrawn Amt must be greater than 0 and less than $${this.bal}(your acct balance)`; 
+            
+            console.log(error);
+            return error;
+        } else {
+            this.bal -= withdrawAmt;
+            return this.bal
+        }
+    }
+    balance() {
+        return this.bal;
+    }
+}
+
+
+const functions = {
+    // nothing in here as of yet
+}
+
+export { Accounts, functions };
